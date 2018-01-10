d7107b363f031f2281a128bc4f2c72806fe7088b
Jansen, Jan <Jan.Jansen@gdata.de>
2018-01-10 12:43:52 +0100

    Prevent overflow by adding a sanity check

---
+++ src/PeNet.Asn1/Asn1ParsingException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace PeNet.Asn1
+{
+    public class Asn1ParsingException : Exception
+    {
+        public Asn1ParsingException(string message) : base(message)
+        {
+        }
+    }
+}
