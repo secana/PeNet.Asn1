f5ef2cddc7525479a30af169c3fe0dc3bc902170
Cédric Luthi <cedric.luthi@gmail.com>
2021-10-24 14:49:12 +0200

    Add Public API approval test
    This makes it easy to spot changes to the public API surface and approve them. The `Verify` NuGet package [automatically copies to the clipboard][1] a command to accept the changes.
    
    [1]: https://github.com/VerifyTests/Verify/blob/main/docs/clipboard.md

---
+++ test/PeNet.Asn1_Test/PublicApi.cs
@@ -0,0 +1,20 @@
+using System.Threading.Tasks;
+using PeNet.Asn1;
+using PublicApiGenerator;
+using VerifyXunit;
+using Xunit;
+
+namespace PeNet.Asn1_Test
+{
+    [UsesVerify]
+    public class PublicApi
+    {
+        [Fact]
+        public Task ApprovePublicApi()
+        {
+            var options = new ApiGeneratorOptions { IncludeAssemblyAttributes = false };
+            var publicApi = typeof(Asn1Node).Assembly.GeneratePublicApi(options);
+            return Verifier.Verify(publicApi).UseFileName("PublicApi");
+        }
+    }
+}
